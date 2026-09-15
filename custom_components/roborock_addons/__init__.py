0cf72e1a5dfe60c1edca527aef61dc51e36b9507
Martin Gøranson <30817386+isimagan@users.noreply.github.com>
2026-09-15 09:33:09 +0200

    Keep setup config-entry based

--- custom_components/roborock_addons/__init__.py
+++ custom_components/roborock_addons/__init__.py
@@ -2,14 +2,6 @@
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
-
-from .const import DOMAIN
-
-
-async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up Roborock Add-ons from YAML."""
-    return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
