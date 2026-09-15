8185dccc3800e15c4e88fcc4190c46964cb42f7e
Martin Gøranson <30817386+isimagan@users.noreply.github.com>
2026-09-15 09:32:18 +0200

    Support config entry setup

--- custom_components/roborock_addons/__init__.py
+++ custom_components/roborock_addons/__init__.py
@@ -1,11 +1,22 @@
 """Roborock Add-ons integration for Home Assistant."""
 
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 
-DOMAIN = "roborock_addons"
+from .const import DOMAIN
 
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up Roborock Add-ons."""
+    """Set up Roborock Add-ons from YAML."""
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Roborock Add-ons from a config entry."""
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a Roborock Add-ons config entry."""
     return True
